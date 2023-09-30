52c9ad9b6287e818920e6cd48c427b08552defa0
adam-lzk <adam.loziuk@gmail.com>
2023-09-30 09:56:09 +0300

    Add &&& selector, now styles work

--- src/atoms.js
+++ src/atoms.js
@@ -2,7 +2,9 @@ import styled from '@emotion/styled'
 import { Layout } from 'antd'
 
 export const StyledSider = styled(Layout.Sider)`
-  height: calc(100% - 64px);  /* viewport height minus header height */
-  position: sticky;
-  top: 64px;
+  &&& {
+    height: calc(100% - 64px);  /* viewport height minus header height */
+    position: sticky;
+    top: 64px;
+  }
 `
